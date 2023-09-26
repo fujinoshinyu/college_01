295e821b54afba27041ccae09472e006e824815d
fujinoshinyu <sincan.1911@gmail.com>
2023-09-26 20:44:42 +0900

    third commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,12 @@ class PostController extends Controller
 {
     public function index(Post $post)
 {
-    return $post->get();
+    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+//getPaginateByLimit()はPost.phpで定義したメソッドです
 }
-}
+
+    public function show(Post $post)
+{
+        return view('post/show')->with(['post'=>$post]);
+    }
+}?>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,12 +9,15 @@
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
-        @foreach($posts as $post)
-            <div class='post'>
-                <h2 class='title'>{{ $post->title}}</h2>
-                <p class='body'>{{ $post->body}}</p>
-            </div>
-        @endforeach
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
