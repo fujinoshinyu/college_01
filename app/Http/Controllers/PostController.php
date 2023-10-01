d4b9ddfcb7386a564ab26b01bc804342d32fb489
fujinoshinyu <sincan.1911@gmail.com>
2023-10-01 23:36:33 +0900

    4 commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -15,7 +16,7 @@ class PostController extends Controller
 
     public function show(Post $post)
 {
-        return view('post/show')->with(['post'=>$post]);
+        return view('posts/show')->with(['post'=>$post]);
     
 }
     public function create()
@@ -23,8 +24,12 @@ class PostController extends Controller
         return view('posts/create');
         
 }
-    
-    
 
+public function store(PostRequest $request, Post $post)
+{
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+}
 }
 ?>
